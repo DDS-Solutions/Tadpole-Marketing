9c23c99df7207378ca4a9c660d7d7cdfba72bd33
Overlord <prowificameraremote@gmail.com>
2026-02-27 18:49:10 -0500

    Configure Astro for GitHub Pages automated deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://DDS-Solutions.github.io',
+    base: '/Tadpole-Marketing',
     integrations: [tailwind()],
 });
